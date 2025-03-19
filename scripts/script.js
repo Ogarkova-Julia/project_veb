c97368bf1733a9f93932f75c48e84c45c8c10385
Ogarkova-Julia <ogarkovaulia07@gmail.com>
2025-03-19 11:50:17 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -21,19 +21,104 @@ document.addEventListener('DOMContentLoaded', () => {
 
        
 
-        // Слушатель для кнопки открытия чата
-        document.getElementById('chat-button').addEventListener('click', function() {
-        console.log('Кнопка "Чат с поддержкой" нажата.');
+    // Слушатель для кнопки открытия чата
+    const chatButton = document.getElementById('chat-button');
     
-        document.getElementById('chat-window').classList.toggle('hidden');
+    if (chatButton) {
+        chatButton.addEventListener('click', function() {
+            console.log('Кнопка "Чат с поддержкой" нажата.');
+            document.getElementById('chat-window').classList.toggle('hidden');
         });
-
-        // Слушатель для кнопки закрытия чата
-        document.getElementById('close-chat').addEventListener('click', function() {
-        console.log('Кнопка закрытия чата нажата.');
-
-        document.getElementById('chat-window').classList.add('hidden');
-        });
-
+    } else {
+        console.log('Кнопка "Чат с поддержкой" не найдена.');
     }
-    );
+
+    // Слушатель для кнопки закрытия чата
+    const closeChatButton = document.getElementById('close-chat');
+
+    if (closeChatButton) {
+        closeChatButton.addEventListener('click', function() {
+            console.log('Кнопка закрытия чата нажата.');
+            document.getElementById('chat-window').classList.add('hidden');
+        });
+    } else {
+        console.log('Кнопка закрытия чата не найдена.');
+    }
+    
+
+
+
+
+    /*3. Появление форм*/
+    /*Алгоритм
+        *
+        *   1. Начало.
+        *   2. Пользователь кликает на кнопку Вход/регистрация сайта.
+        *   3. На экране появляется форма с заполнением логина и пароля для входа в аккаунт пользователя.
+        *   4. Условие: Пользователь зарегестрирован на сайте?:
+        *       1. да: заполняет появившиеся поля
+        *       2. нет: переходит на форму Зарегистрироваться с помощью нажатия на "Нет аккаунта? Зарегистрируйтесь", и заполняет появившуюся форму.
+        *       и наоборот
+        *   5. Конец
+        * 
+        *   Блок-схема: /images/block-schema(2).png
+        */
+
+
+    //Для формы зарегистрироваться//
+    const welcоmeButtonModal = document.querySelector(".services__button");
+    const modalApplication = document.querySelector(".applications");
+    if (welcоmeButtonModal&&modalApplication) {
+        //Для кнопки «Записаться на курс» добавляем обработчик события клика по этой кнопке:
+            welcоmeButtonModal.addEventListener("click", () => {
+        // удаляем атрибут hidden у модального окна modalApplication и модальное окно становится видимым
+           modalApplication.removeAttribute("hidden");
+           });
+        }
+        
+        // добавляем обработчик события при клике вне области формы. Тогда каждый раз, когда пользователь кликает где-либо на фоне вокруг появившейся формы, будет вызываться функция,
+        window.addEventListener("click", (event) => {
+        // проверяем, был ли клик на фоне модального окна
+          if (event.target === modalApplication) {
+        //если условие выполняется, добавляем атрибут hiddenу модального окна modalApplicationи модальное окно становится невидимым
+            modalApplication.setAttribute("hidden", true);
+            }
+        });
+        const closeModalButton = document.querySelector(".application__close");
+
+        //Для кнопки «Закрыть» добавляем обработчик события клика по этой кнопке:
+        closeModalButton.addEventListener("click", () => {
+        // Добавляем атрибут hidden у модального окна modalApplication и модальное окно становится невидимым
+        modalApplication.setAttribute("hidden", true);
+    });
+
+        
+//для формы войти//
+const welcоmeButton = document.querySelector(".header__login");
+const modalApplic = document.querySelector(".applications__login");
+if (welcоmeButton&&modalApplic) {
+    //Для кнопки «Вход» добавляем обработчик события клика по этой кнопке:
+        welcоmeButton.addEventListener("click", () => {
+    // удаляем атрибут hidden у модального окна modalApplic и модальное окно становится видимым
+       modalApplic.removeAttribute("hidden");
+       });
+    }
+    
+    // добавляем обработчик события при клике вне области формы. Тогда каждый раз, когда пользователь кликает где-либо на фоне вокруг появившейся формы, будет вызываться функция,
+    window.addEventListener("click", (event) => {
+    // проверяем, был ли клик на фоне модального окна
+      if (event.target === modalApplic) {
+    //если условие выполняется, добавляем атрибут hiddenу модального окна modalApplic модальное окно становится невидимым
+        modalApplication.setAttribute("hidden", true);
+        }
+    });
+    const closeModal = document.querySelector(".application__close__login");
+
+//Для кнопки «Закрыть» добавляем обработчик события клика по этой кнопке:
+closeModal.addEventListener("click", () => {
+// Добавляем атрибут hidden у модального окна modalApplic и модальное окно становится невидимым
+modalApplic.setAttribute("hidden", true);
+});
+
+}
+);
